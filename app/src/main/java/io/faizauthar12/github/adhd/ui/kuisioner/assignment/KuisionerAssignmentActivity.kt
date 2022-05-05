89048a129715ffafbe6bbd01e6abcb9afb82ce04
Faiz Authar <faizauthar@gmail.com>
2022-05-06 06:57:00 +0700

    ui: Initialize Kuisioner Assigment Activity

---
+++ app/src/main/java/io/faizauthar12/github/adhd/ui/kuisioner/assignment/KuisionerAssignmentActivity.kt
@@ -0,0 +1,16 @@
+package io.faizauthar12.github.adhd.ui.kuisioner.assignment
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.faizauthar12.github.adhd.databinding.ActivityKuisionerAssignmentBinding
+
+class KuisionerAssignmentActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityKuisionerAssignmentBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityKuisionerAssignmentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
