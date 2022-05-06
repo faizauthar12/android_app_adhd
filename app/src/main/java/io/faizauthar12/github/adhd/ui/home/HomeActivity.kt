df6024d4d37f5985a2b8982d78e5888fdaab1fcb
Faiz Authar <faizauthar@gmail.com>
2022-05-06 07:09:27 +0700

    ui: Home: add and route button to kuisioner activity

--- app/src/main/java/io/faizauthar12/github/adhd/ui/home/HomeActivity.kt
+++ app/src/main/java/io/faizauthar12/github/adhd/ui/home/HomeActivity.kt
@@ -1,8 +1,10 @@
 package io.faizauthar12.github.adhd.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import io.faizauthar12.github.adhd.databinding.ActivityHomeBinding
+import io.faizauthar12.github.adhd.ui.kuisioner.assignment.KuisionerAssignmentActivity
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
@@ -12,5 +14,11 @@ class HomeActivity : AppCompatActivity() {
 
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            Intent(this, KuisionerAssignmentActivity::class.java).apply {
+                startActivity(this)
+            }
+        }
     }
 }
