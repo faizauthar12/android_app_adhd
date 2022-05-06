fa72fcea5745731342f1529965eec9f8397b586a
Faiz Authar <faizauthar@gmail.com>
2022-05-06 18:58:52 +0700

    utils: Initial assignment dummy data

---
+++ app/src/main/java/io/faizauthar12/github/adhd/utils/dummy/AssignmentDummy.kt
@@ -0,0 +1,290 @@
+package io.faizauthar12.github.adhd.utils.dummy
+
+import io.faizauthar12.github.adhd.data.local.entity.AssignmentEntity
+
+object AssignmentDummy {
+    fun generateAssignmentDummy(): List<AssignmentEntity> {
+        val assignments = ArrayList<AssignmentEntity>()
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 1,
+                assignmentCode = "G001",
+                assignmentDesc = "Gagal memberikan perhatian penuh pada detail."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 2,
+                assignmentCode = "G002",
+                assignmentDesc = "Sering mengganggu kegiatan orang lain."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 3,
+                assignmentCode = "G003",
+                assignmentDesc = "Sulit memberikan perhatian saat bermain."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 4,
+                assignmentCode = "G004",
+                assignmentDesc = "Tidak suka kegiatan-kegiatan yang membutuhkan usaha berkelanjutan contoh bermain puzzle/lego."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 5,
+                assignmentCode = "G005",
+                assignmentDesc = "Tidak bisa mengikuti instruksi yang diberikan."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 6,
+                assignmentCode = "G006",
+                assignmentDesc = "Kesulitan dalam mengorganisasikan tugas dan kegiatan (contoh: menaruh sepatu lalu menaruh barang lalu membereskan tempat tidur)."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 7,
+                assignmentCode = "G007",
+                assignmentDesc = "Menghindari permainan yang perlu usaha mental seperti membuat PR atau tugas sekolah."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 8,
+                assignmentCode = "G008",
+                assignmentDesc = "Mudah Takut, mudah was-was"
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 9,
+                assignmentCode = "G009",
+                assignmentDesc = "Menjawab pertanyaan tanpa berpikir, sementara pertanyaan yang diajukan belum selesai."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 10,
+                assignmentCode = "G010",
+                assignmentDesc = "Lebih sering berjalan-jalan dan tidak dapat bermain dengan tenang."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 11,
+                assignmentCode = "G011",
+                assignmentDesc = "Bicara berlebihan."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 12,
+                assignmentCode = "G012",
+                assignmentDesc = "Sering menggerak-gerakkan kaki ketika duduk diam."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 13,
+                assignmentCode = "G013",
+                assignmentDesc = "Ceroboh mengerjakan tugas di rumah atau kegiatan lain."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 14,
+                assignmentCode = "G014",
+                assignmentDesc = "Tidak sabar dalam menunggu gilirannya (tidak sabaran)."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 15,
+                assignmentCode = "G015",
+                assignmentDesc = "Sering meminta mainan teman secara paksa."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 16,
+                assignmentCode = "G016",
+                assignmentDesc = "Reaktif, atau sering membalas kembali apa yang dilakukan kepadanya."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 17,
+                assignmentCode = "G017",
+                assignmentDesc = "Sering mengulang-ulang kata yang telah diucapkan oleh teman."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 18,
+                assignmentCode = "G018",
+                assignmentDesc = "Sering bertindak menggangu teman."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 19,
+                assignmentCode = "G019",
+                assignmentDesc = "Memiliki sikap tidak patuh dan membangkang."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 20,
+                assignmentCode = "G020",
+                assignmentDesc = "Sering tidak menaati peraturan, bahkan peraturan yang sederhana."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 21,
+                assignmentCode = "G021",
+                assignmentDesc = "Tidak mengikuti instruksi."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 22,
+                assignmentCode = "G022",
+                assignmentDesc = "Mudah terganggu, serta mudah marah."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 23,
+                assignmentCode = "G023",
+                assignmentDesc = "Tidak percaya diri."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 24,
+                assignmentCode = "G024",
+                assignmentDesc = "Kehilangan barang barang."
+            )
+        )
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 25,
+                assignmentCode = "G025",
+                assignmentDesc = "Mudah teralihkan perhatiannya."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 26,
+                assignmentCode = "G026",
+                assignmentDesc = "Tidak memperhatikan ketika seseorang sedang berbicara."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 27,
+                assignmentCode = "G027",
+                assignmentDesc = "Tidak dapat mengatur tugas / kegiatan sehari-hari."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 28,
+                assignmentCode = "G028",
+                assignmentDesc = "Sulit mengikuti arahan atau petunjuk."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 29,
+                assignmentCode = "G029",
+                assignmentDesc = "Sangat peka atau sensitif pada kritikan."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 30,
+                assignmentCode = "G030",
+                assignmentDesc = "Seringkali lupa terhadap kebiasan dan kegiatan sehari-hari."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 31,
+                assignmentCode = "G031",
+                assignmentDesc = "Tidak bisa diam, seperti berjalan atau memanjat."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 32,
+                assignmentCode = "G032",
+                assignmentDesc = "Sering menggeliat."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 33,
+                assignmentCode = "G033",
+                assignmentDesc = "Sering membuat ribut."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 34,
+                assignmentCode = "G034",
+                assignmentDesc = "Selalu ingin memegang benda yang dilihat."
+            )
+        )
+
+        assignments.add(
+            AssignmentEntity(
+                assignmentId = 35,
+                assignmentCode = "G035",
+                assignmentDesc = "Tidak membutuhkan waktu tidur / istirahat, karena memiliki energi yang berlebih."
+            )
+        )
+
+        return assignments
+    }
+}
