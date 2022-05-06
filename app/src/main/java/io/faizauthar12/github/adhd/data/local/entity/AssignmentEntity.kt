627ef73ec49ea5799f8a0e30c872e8a00e24089e
Faiz Authar <faizauthar@gmail.com>
2022-05-06 18:32:47 +0700

    data: Initial Assignment Entity

---
+++ app/src/main/java/io/faizauthar12/github/adhd/data/local/entity/AssignmentEntity.kt
@@ -0,0 +1,7 @@
+package io.faizauthar12.github.adhd.data.local.entity
+
+data class AssignmentEntity(
+    val assignmentId: Int,
+    val assignmentCode: String,
+    val assignmentDesc: String,
+)
