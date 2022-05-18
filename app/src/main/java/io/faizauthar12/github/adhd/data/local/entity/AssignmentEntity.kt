ac1bfd764359aa22104d2dd49bd500fcaf36a0c7
Faiz Authar <faizauthar@gmail.com>
2022-05-18 10:40:20 +0700

    entity: preparation for ROOM database

--- app/src/main/java/io/faizauthar12/github/adhd/data/local/entity/AssignmentEntity.kt
+++ app/src/main/java/io/faizauthar12/github/adhd/data/local/entity/AssignmentEntity.kt
@@ -1,7 +1,20 @@
 package io.faizauthar12.github.adhd.data.local.entity
 
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "AssignmentEntity")
 data class AssignmentEntity(
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    @ColumnInfo(name = "assignmentId")
     val assignmentId: Int,
+
+    @ColumnInfo(name = "assignmentCode")
     val assignmentCode: String,
+
+    @ColumnInfo(name = "assignmentDesc")
     val assignmentDesc: String,
 )
