836d89fad277faf326e2cdeec6a67da33260ce63
Faiz Authar <faizauthar@gmail.com>
2022-06-22 14:38:42 +0700

    entity: Kotlin parcelize

--- app/src/main/java/io/faizauthar12/github/adhd/data/local/entity/AssignmentEntity.kt
+++ app/src/main/java/io/faizauthar12/github/adhd/data/local/entity/AssignmentEntity.kt
@@ -1,8 +1,12 @@
 package io.faizauthar12.github.adhd.data.local.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class AssignmentEntity(
     val assignmentId: Int,
     val assignmentCode: String,
     val assignmentDesc: String,
     var assignmentAnswer: String? = null,
-)
+) : Parcelable
